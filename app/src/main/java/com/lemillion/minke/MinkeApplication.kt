859c2bbd71fd03353890ac6d65f4221ae0245251
Sanyam Mehta <sanyam.mehta12@gmail.com>
2021-11-01 16:38:27 +0530

    Renaming MainApplication to MinkeApplication

--- app/src/main/java/com/lemillion/minke/MinkeApplication.kt
+++ app/src/main/java/com/lemillion/minke/MinkeApplication.kt
@@ -2,10 +2,17 @@ package com.lemillion.minke
 
 import android.app.Application
 import androidx.work.Configuration
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application(), Configuration.Provider {
+class MinkeApplication : Application(), Configuration.Provider {
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+
     override fun getWorkManagerConfiguration(): Configuration =
         Configuration.Builder()
             .setMinimumLoggingLevel(if (BuildConfig.DEBUG) android.util.Log.DEBUG else android.util.Log.ERROR)
